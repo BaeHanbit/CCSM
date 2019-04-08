d48327c23a46b24cb526e0a765ad3f2f75f4a66a
Bae hanbit <haun24ne@gmail.com>
2019-04-08 10:18:42 +0900

    [Add] Header_file

---
+++ Core/header.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma warning(disable:4996)
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <list>
+
+#include <time.h>
+#include <Windows.h>
+#include <direct.h>
+#include <io.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <errno.h>
